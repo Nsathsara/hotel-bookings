fc83a079486ef2499fd8e6e593139959fa6a648f
Nipun Sathsara <161104232+Nsathsara@users.noreply.github.com>
2025-05-27 10:51:01 +0530

    error fixed

--- server/controllers/clerkWebhooks.js
+++ server/controllers/clerkWebhooks.js
@@ -3,7 +3,7 @@ import { Webhook} from "svix";
 
 const clerkWebhooks = async (req, res)=> {
     try {
-        const whook = new Webhook(process.end.CLERK_WEBHOOK_SECRET)
+        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
 
 
         //getting headers
